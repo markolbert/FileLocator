7b8d1a8ec5ca7cae1afe59f39ffe1934f7fcc894
Mark Olbert <mark@arcabama.com>
2024-05-15 09:51:48 -0700

    unneeded

--- FileUtilities/IConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace J4JSoftware.FileUtilities;
-
-public interface IConfiguration
-{
-    List<ImportDirectory> ImportDirectories { get; set; }
-    StyleConfiguration Styles { get; set; }
-}
