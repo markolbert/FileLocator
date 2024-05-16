e340a8dadacbde1a81e1ab824631e58ac2f0fffb
Mark Olbert <mark@arcabama.com>
2024-05-16 10:39:20 -0700

    move from derived assembly

---
+++ FileUtilities/interfaces/ITableSource.cs
@@ -0,0 +1,17 @@
+﻿namespace J4JSoftware.FileUtilities;
+
+public interface ITableSource : IFileContext
+{
+    Type EntityType { get; }
+    string FileType { get; }
+
+    string? TweakPath { get; set; }
+}
+
+public interface ICsvTableSource : ITableSource, ICsvContext
+{
+}
+
+public interface IWorksheetTableSource : ITableSource, IWorkbookContext
+{
+}
