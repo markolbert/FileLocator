b77a327a869ded4f813d6d73c66211edc9fc4974
Mark Olbert <mark@arcabama.com>
2024-07-27 13:51:05 -0700

    add comments for doc tests

--- FileUtilities/converters/csv/CsvDateOnlyFormatter.cs
+++ FileUtilities/converters/csv/CsvDateOnlyFormatter.cs
@@ -5,12 +5,19 @@ using Microsoft.Extensions.Logging;
 
 namespace J4JSoftware.FileUtilities;
 
+/// <summary>
+/// This is a test structured comment
+/// </summary>
+/// <param name="loggerFactory">This is a test structured comment param node</param>
 public class CsvDateOnlyFormatter( ILoggerFactory? loggerFactory ) : DefaultTypeConverter
 {
+    // this is a plain old comment on a field
     private readonly ILogger? _logger = loggerFactory?.CreateLogger<CsvDateOnlyFormatter>();
 
+    // this is a plain old comment on a method
     public override object ConvertFromString( string? text, IReaderRow row, MemberMapData memberMapData )
     {
+        // this is a plain old comment inside a method
         if( DateTime.TryParse( text, out var retVal ) )
             return retVal.Date;
 
