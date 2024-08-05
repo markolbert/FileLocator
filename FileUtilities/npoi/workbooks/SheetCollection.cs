4083ad701d119ebba87d44c3ab4f5b1d84ba26fa
Mark Olbert <mark@arcabama.com>
2024-08-05 10:13:35 -0700

    reorganize files

---
+++ FileUtilities/npoi/workbooks/SheetCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace J4JSoftware.FileUtilities;
+
+internal class SheetCollection() : KeyedCollection<string, ISheetCreator>( StringComparer.OrdinalIgnoreCase )
+{
+    protected override string GetKeyForItem( ISheetCreator item ) => item.SheetName;
+}
