c99ebedf4d5e8d07ccab9b753c9d6d8f538337ea
Mark Olbert <mark@arcabama.com>
2025-01-02 19:01:20 -0800

    add converters for text

---
+++ FileUtilities/converters/csv/CsvTrimmedTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace J4JSoftware.FileUtilities;
+
+public class CsvTrimmedTextConverter : DefaultTypeConverter
+{
+    public override object? ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData) =>
+        string.IsNullOrWhiteSpace(text) ? string.Empty : text.Trim();
+
+    public override string? ConvertToString(object? value, IWriterRow row, MemberMapData memberMapData) =>
+        value switch
+        {
+            null => string.Empty,
+            string textValue => textValue.Trim(),
+            _ => value.ToString()
+        };
+}
