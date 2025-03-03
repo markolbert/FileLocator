962c7db9b5dc08bab84b7aa6a171dd5f2c4e89b8
Mark Olbert <mark@arcabama.com>
2025-03-03 12:35:09 -0800

    rename for consistency

--- src/npoi/converters/NpoiFieldAttribute.cs
+++ src/npoi/converters/NpoiFieldAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace J4JSoftware.FileUtilities;
 
 [AttributeUsage(AttributeTargets.Property)]
-public class NpoiMappingAttribute( string npoiFieldName, Type? converterType = null ) : Attribute
+public class NpoiFieldAttribute( string npoiFieldName, Type? converterType = null ) : Attribute
 {
     public string NpoiFieldName { get; } = npoiFieldName;
     public Type? ConverterType { get; } = converterType;
