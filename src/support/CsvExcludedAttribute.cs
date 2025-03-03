cf8ef68f8c54b0d2c708a66075fb7682eaedbe9f
Mark Olbert <mark@arcabama.com>
2025-03-03 12:37:02 -0800

    obsolete

--- src/support/CsvExcludedAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace J4JSoftware.FileUtilities;
-
-[ AttributeUsage( AttributeTargets.Class, Inherited = false ) ]
-public class CsvExcludedAttribute( params string[] excludedProperties ) : Attribute
-{
-    public string[] ExcludedProperties { get; } = excludedProperties;
-}
